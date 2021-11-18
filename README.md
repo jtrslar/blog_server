500d37689a45001203611cc94008752d806b6426
jtrslar <41978969+jtrslar@users.noreply.github.com>
2021-11-19 00:00:38 +0800

    完成第一个测试接口 (#1)

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # blog_server
+
+## 项目描述
+
+    >>> 基于koa2框架为前端项目提供博客列表接口
+
+## 使用说明
+
+```bash
+# 安装依赖
+npm install
+
+# 项目运行
+npm start
+
+```
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,53 @@
 const router = require('koa-router')()
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+// 暂时假装是真实数据
+const resData = [
+  {
+    id: 1,
+    file: 'test1.md',
+    name: '测试1MD'
+  },
+  {
+    id: 2,
+    file: 'test2.md',
+    name: '测试2MD'
+  },
+  {
+    id: 3,
+    file: 'test3.md',
+    name: '测试3MD'
+  },
+  {
+    id: 4,
+    file: 'test4.md',
+    name: '测试4MD'
+  },
+  {
+    id: 5,
+    file: 'test5.md',
+    name: '测试5MD'
+  },
+  {
+    id: 6,
+    file: 'test6.md',
+    name: '测试6MD'
+  },
+]
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
+router.post('/file/md', async (ctx, next) => {
+  const params = ctx.request.body
+ 
+  const {pageNum, pageSize} = params
+  if(pageNum && pageSize){
+    let res = resData.slice(pageSize*(pageNum-1), pageSize*pageNum)
+    console.log('res loading is ===>>> ', res)
+    ctx.body = {
+      code: 200,
+      data: res,
+      msg: 'success'
+    }
+  }else{
+    ctx.response.status = 500
   }
 })
 
