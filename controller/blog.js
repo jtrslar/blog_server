8851a0b26cb276e3ed8424c6eb1c2d7da6b96936
jtrslar <41978969+jtrslar@users.noreply.github.com>
2021-11-25 00:22:36 +0800

    完成博客详情获取接口 (#4)

--- controller/blog.js
+++ controller/blog.js
@@ -1,4 +1,6 @@
 const blogService = require('../service/blog')
+const koaReq = require('koa2-request')
+const marked = require('marked')
 
 const getBlogList = async (ctx) => {
   const { pageNum, pageSize } = ctx.request.body
@@ -12,6 +14,28 @@ const getBlogList = async (ctx) => {
   ctx.body = await blogService.getBlogList(pageNum, pageSize)
 }
 
+const getBlogById = async (ctx)=>{
+  const {id} = ctx.request.body
+  if(!id){
+    ctx.body = {
+      code: 500,
+      msg: '参数错误',
+    }
+    return
+  }
+
+  const {dataValues} = await blogService.getBlogById(id)
+    // markdown文件的链接前缀(暂时使用假链接)
+  const mdFileUrl = `http://121.5.23.189:8010/file/md/${dataValues.file}`
+  const {body:fileDetail} = await koaReq({
+    url: mdFileUrl,
+    method: 'get',
+  })
+  dataValues.fileDetail = fileDetail
+  ctx.body = dataValues
+}
+
 module.exports = {
   getBlogList,
+  getBlogById
 }
--- service/blog.js
+++ service/blog.js
@@ -2,15 +2,20 @@ const blogModel = require('../models/blog')
 
 // 查询bloglist(分页)
 const getBlogList = async (pageNum, pageSize) => {
-  if (!pageNum || !pageSize) {
-    return []
-  }
   return await blogModel.findAll({
     offset: pageSize * (pageNum - 1),
     limit: pageSize,
   })
 }
 
+// 根据id查询blog
+const getBlogById = async (id) => {
+  return await blogModel.findOne({
+    where: {id}
+  })
+}
+
 module.exports = {
   getBlogList,
+  getBlogById
 }
