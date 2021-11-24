917e3b43bb15079a4f316e2e13b263d7a5fee4f6
夜观天象 <jtrslar@163.com>
2021-11-25 00:22:29 +0800

    完成博客详情获取接口

--- models/blog.js
+++ models/blog.js
@@ -1,22 +1,16 @@
 const sequelize = require('./sequelize')
 const Sequelize = require('sequelize')
 
-const Blog = sequelize.define(
-  'blog',
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
-    file: Sequelize.STRING,
-    title: Sequelize.STRING,
+const Blog = sequelize.define('blog', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
   },
-  {
-    timestamps: false,
-  }
-)
-
-Blog.sync()
+  file: Sequelize.STRING,
+  title: Sequelize.STRING,
+},{
+  timestamps: false
+})
 
 module.exports = Blog
--- routes/blog.js
+++ routes/blog.js
@@ -4,5 +4,8 @@ const blogController = require('../controller/blog')
 router.prefix('/api/blog')
 
 router.post('/list', blogController.getBlogList)
+router.post('/detail', blogController.getBlogById)
+
+
 
 module.exports = router
